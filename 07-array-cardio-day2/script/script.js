012325293a2bb9818136d8eacd5a266e58ec7fd1
harunugurlu <harunugurlu1@gmail.com>
2022-04-30 15:23:55 +0300

    finish

--- 07-array-cardio-day2/script/script.js
+++ 07-array-cardio-day2/script/script.js
@@ -17,12 +17,63 @@
 
  // Some and Every Checks
  // Array.prototype.some() // is at least one person 19 or older?
+ console.log('Some and Every Checks');
+ const currentYear = (new Date()).getFullYear();
+
+ //1st solution:
+ var isAdult = people.some((person) => currentYear - person.year >= 19);
+ console.log(isAdult);
+ //Alt
+ isAdult = people.some((person) => {
+     if (currentYear - person.year >= 19) return true;
+     else return false;
+ });
+ console.log(isAdult);
+
+ console.log(people.some((person) =>
+     person.name === 'Lux'));
+
  // Array.prototype.every() // is everyone 19 or older?
+ //1st solution:
+ var allAdults = people.every(function(person) {
+     if (currentYear - person.year >= 19) return true;
+     else return false;
+ });
+ console.log(allAdults);
+
+ //Alt
+ allAdults = people.every((person) => currentYear - person.year >= 19);
+ console.log(allAdults);
 
  // Array.prototype.find()
+ console.log('Array.prototype.find()');
  // Find is like filter, but instead returns just the one you are looking for
  // find the comment with the ID of 823423
+ var comment = comments.find(comment => comment.id == 823423);
+ console.log(comment);
 
+ comment = comments.find(function(comment) {
+     if (comment.id == 823423) return true;
+     else return false;
+ });
+ console.log(comment);
  // Array.prototype.findIndex()
+ console.log('Array.prototyp.findIndex()');
  // Find the comment with this ID
+ const comWithThatId = comments[comments.findIndex((comment) => comment.id == 523423)];
+ console.log(comWithThatId);
  // delete the comment with the ID of 823423
+ console.log('Deleting');
+ const index = comments.findIndex(function(comment) {
+     if (comment.id == 823423) return true;
+     else return false;
+ });
+ //Alt
+ const newComments = [
+     ...comments.slice(0, index),
+     ...comments.slice(index + 1)
+ ];
+ console.table(newComments);
+
+ comments.splice(index, 1);
+ console.table(comments);
